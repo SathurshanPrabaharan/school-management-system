b93dcb0393055a75581a0baa5dd087bf801da485
sathurshanath <sathurshanath2021@gmail.com>
2023-11-25 06:50:20 +0530

    Create About Us page

---
+++ about-us.php
@@ -0,0 +1,49 @@
+<?php include "./inc/top.php"; ?>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-12 mt-2">
+            <?php 
+                include "./inc/navbar.php" ; 
+                // include "./inc/slider.php" ;
+            ?>
+        </div>
+    </div>
+
+    <div class="row mt-2">
+        <div class="col-md-9">
+            <div class="card my-2">
+                <div class="card-header bg-primary text-white">About Lamda Academy</div>
+                <div class="card-body text-justify">
+                    <p class="card-text">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus tempore quos voluptate
+                         perspiciatis neque quibusdam placeat doloribus officiis nesciunt nisi tempora, nihil, culpa sint 
+                         numquam. Quis illum veritatis molestias, adipisci excepturi nisi tempore voluptatum optio architecto 
+                         laudantium dicta! Quam consectetur nostrum beatae eum consequuntur eveniet suscipit molestiae, odit 
+                         ex velit ipsa voluptatibus perspiciatis. Accusamus nemo esse id dolor corporis dignissimos fugit magni 
+                         qui, eos deserunt porro itaque error molestias distinctio rem quasi unde magnam, modi, consequuntur 
+                         optio blanditiis iure corrupti repellendus. Consequuntur maxime repellat nobis inventore repudiandae
+                         fugit enim, quidem odio molestias sit nesciunt aperiam veritatis possimus cumque rerum ipsam.
+                    </p>
+                    <a href="gallery.php" class="btn btn-primary">View Gallery</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card bg-warning text-white my-2">
+                <div class="card-body">
+                    <h4 class="card-title text-center">Parents Review</h4>
+                </div>
+                <img src="./images/004.jpg" class="img-fluid">
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid ">
+        <div class="row bg-dark py-5">
+            <?php include "./inc/footer.php" ; ?>
+        </div>
+    </div>
+
+
+</div>
