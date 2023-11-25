2b5c459dd744b8d28e45de6e836ea373b2d82e23
sathurshanath <sathurshanath2021@gmail.com>
2023-11-25 06:28:08 +0530

    create slider

--- index.php
+++ index.php
@@ -5,12 +5,15 @@
 <div class="container-fluid">
     <div class="row">
         <div class="col-lg-12 mt-2">
-            <?php include "./inc/navbar.php" ; ?>
+            <?php 
+                include "./inc/navbar.php" ; 
+                include "./inc/slider.php" ;
+            ?>
         </div>
     </div>
 
-    <div class="container-fluid">
-        <div class="row bg-dark">
+    <div class="container-fluid ">
+        <div class="row bg-dark py-5">
             <?php include "./inc/footer.php" ; ?>
         </div>
     </div>
